886d6b7700aca019d61705c3befcd63d8b4e39a7
Stephane Le Solliec <slesolliec@gmail.com>
2018-01-09 11:25:51 +0100

    Reading switch states

--- main.js
+++ main.js
@@ -10,22 +10,40 @@ configs = JSON.parse(configs);
 // url of Domoticz JSON API protected by username / password
 const url = configs.domoticz
     + "/json.htm"
-    + "?username=" + Buffer.from( configs.username ).toString('base64')
-    + "&password=" + Buffer.from( configs.password ).toString('base64')
-    + "&type=devices&used=true&filter=temp";
+    + "?username=" + Buffer( configs.username ).toString('base64')
+    + "&password=" + Buffer( configs.password ).toString('base64')
+    + "&type=devices&used=true";
+// console.log(url);
+
+var tempData     = '';
+var switchesData = '';
 
 
-// get temperatures from Domoticz
-https.get(url, function(res) {
+// get switch states from Domoticz
+https.get(url + '&filter=light', function(res) {
     res.setEncoding("utf8");
-    let body = "";
-    res.on("data", function(data) { body += data; });
+    res.on("data", function(data) { switchesData += data; });
     res.on("end",  function() {
-        // console.log(body);
-        body = JSON.parse(body);
-        body.result.forEach( showTemp )
+        // console.log(switchesData);
+        switchesData = JSON.parse(switchesData);
+        switchesData.result.forEach( function (item) {
+                console.log( item.Name + ' is ' + item.Data + ' (idx=' + item.idx + ')'  );
+            });
     });
 });
+
+// get temperatures from Domoticz
+https.get(url + '&filter=temp', function(res) {
+    res.setEncoding("utf8");
+    res.on("data", function(data) { tempData += data; });
+    res.on("end",  function() {
+        // console.log(body);
+        tempData = JSON.parse(tempData);
+        tempData.result.forEach( showTemp )
+    });
+});
+
+
 
 
 const houseState = getState( new Date() );
@@ -51,20 +69,23 @@ function getState( now ) {
     // Gone?
     // todo: test if we are all gone
 
-    // Night?
-    if ((now.getHours() < 5 ) || (now.getHours() === 6)  && (now.getMinutes() < 30))
-        return 'night';
-    if ((now.getHours() > 22) || (now.getHours() === 22) && (now.getMinutes() > 30))
-        return 'night';
-
     // Home day or Work day?
     switch ( now.getDay() ) {
         case 1:
         case 2:
+            // working days
+            if ((now.getHours() < 5 ) || (now.getHours() > 21) || (now.getHours() === 21) && (now.getMinutes() > 30))
+                return 'night';
+
             if ((now.getHours() > 7 ) || (now.getHours() === 7) && (now.getMinutes() > 30))
                 if (now.getHours() < 17 )
                     return 'out';
             break;
+
+        default:
+            // normal day
+            if ((now.getHours() < 8 ) || (now.getHours() > 21))
+                return 'night';
     }
 
     return 'day';
@@ -95,3 +116,4 @@ function getWantedTemp( room, state) {
 ['parents','living','kitchen','bath'].forEach( function(item) {
     console.log( "Wanted temp is " + getWantedTemp(item,houseState) + " for "+ item);
 });
+
